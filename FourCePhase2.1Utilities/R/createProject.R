60b7ef04405a54ca1d166026aaf5e01d9f38a62f
Nathan Palmer <nathan_palmer@hms.harvard.edu>
2020-09-28 17:44:47 -0400

    removed errant blank line in function body of emitted runAnalysis

--- FourCePhase2.1Utilities/R/createProject.R
+++ FourCePhase2.1Utilities/R/createProject.R
@@ -191,7 +191,6 @@ emit4CeFunctionStubToFile <- function (projectName, functionName, commentPreambl
 createPhase2.1Stubs <- function (projectName, workingDirectory, privateSummariesRepositoryName, publicSummariesRepositoryName) {
 
     runAnalysisFunctionBody = '
-
     ## make sure this instance has the latest version of the quality control and data wrangling code available
     devtools::install_github("https://github.com/covidclinical/Phase2.1DataRPackage", subdir="FourCePhase2.1Data", upgrade=FALSE)
 
